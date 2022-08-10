2c615d0abdd0b7709bea7c34ed5818d13d8511b0
larissa-franciara <larissafranciaradealmeida@gmail.com>
2022-08-10 17:34:13 -0300

    Adição do tratamento de erro genérico

--- aplication/src/db.ts
+++ aplication/src/db.ts
@@ -1,6 +1,6 @@
 import  { Pool } from 'pg';
 
-const connectionString = '';
+const connectionString = 'postgres://jfbeswxt:LEefnWgp5l1oEN86mlIXGtnJKIUL9Xnu@tuffi.db.elephantsql.com/jfbeswxt';
 const db = new Pool({ connectionString })
 
 export default db;
--- aplication/src/index.ts
+++ aplication/src/index.ts
@@ -1,6 +1,7 @@
 import express from 'express';
 import usersRoute from "./routes/users.route";
 import statusRoute from "./routes/status.route";
+import errorHandler from "./middlewares/error-handler.middleware";
 
 const app = express();
 
@@ -13,8 +14,11 @@ app.use(express.urlencoded({ extended: true }));
 app.use(statusRoute);
 app.use(usersRoute);
 
+// Configuração dos Handlers de error
+app.use(errorHandler);
+
 
 // Inicialização do Servidor
 app.listen(3000, () => {
-    console.log('Aplicação executando na porta 3000 !')
+    console.log('Aplicação executando na porta 3000 !');
 })
